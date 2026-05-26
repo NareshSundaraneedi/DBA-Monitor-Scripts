8dd25371bd77516f3b43a4fe1a5019f59334d5a0
NareshSundaraneedi <nareshsundaraneedi@gmail.com>
2026-05-26 12:38:52 +0530

    New Pull request for DBA-Monitor-Scripts

---
+++ Enable tracing for a listener.sql
@@ -0,0 +1,10 @@
+- Set to the listener you want to trace
+
+LSNRCTL> set cur LISTENER_TEST
+
+-- Enable Trace:
+
+LSNRCTL> set trc_level ADMIN
+Connecting to (DESCRIPTION=(ADDRESS=(PROTOCOL=IPC)(KEY=LISTENER_TEST)))
+LISTENER_TEST parameter "trc_level" set to admin
+The command completed successfully
